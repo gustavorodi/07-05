3e55444e17e60f311bc399ca17f520f425e9ffd0
gustavorodi <37717445+gustavorodi@users.noreply.github.com>
2019-05-08 06:54:29 -0300

    Pesquisa em ".txt" pronta

--- Main.java
+++ Main.java
@@ -1,17 +1,42 @@
-public class Main{
-    public static void main(String[] args) {        
-    RabinKarp testeRabinKarp = new RabinKarp("O mundo preciso de mais amor","mundo");
-    Naive testNaive = new Naive("O mundo precisa de mais amor","e");
-      
-       
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws  IOException {
+        RabinKarp testeRabinKarp = new RabinKarp("Play");
+        Naive testNaive = new Naive("O mundo precisa de mais amor", "e");
+
+
         System.out.println(testNaive.searchFile());
-        
-        if(testeRabinKarp.bucascando()){
+
+        if (testeRabinKarp.ler()) {
             System.out.println("Achei");
-        }else{
+        } else {
             System.out.println("Não achei");
         }
-     
     }
-   
+
 }
+
+/*
+ * package scanner.text; import java.io.FileNotFoundException; import
+ * java.io.FileReader; import java.util.Scanner;
+ * 
+ * public class Exemplo02 {
+ * 
+ * public static void main(String args[]) throws FileNotFoundException { Scanner
+ * scanner = new Scanner(new
+ * FileReader("C:\\Users\\lucas.bortolini\\workspace\\Exemplo\\arquivo.txt"));
+ * scanner.useDelimiter("\\n");
+ * 
+ * while (scanner.hasNext()) { System.out.println( scanner.next() ); }
+ * scanner.close();
+ * 
+ * }
+ * 
+ * } }
+ * 
+ * }
+ */
--- RabinKarp.java
+++ RabinKarp.java
@@ -1,54 +1,69 @@
 /*package Rabin_Karp;*/
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import 	java.util.List;
+import java.util.Map;
 public class RabinKarp {
 
   private Integer tamanhoPalavra;
   private Integer tamanhoFrase;
-  private Integer multipicado ;
+  private Integer multipicado;
   private Integer valorPalavra;
   private Integer valorFrase;
-  private String  temp;
-  public  String  texto;
-  public  String  palavra;
-  
+  private String temp;
+  public String palavra;
 
-  public RabinKarp(String texto,String palavra){
-    this.texto    = texto;
-    this.palavra  = palavra;
+  public RabinKarp(String palavra) {
+    this.palavra = palavra;
   }
 
-/*Pulo do gato*/
-  public Boolean bucascando() {
+  /* Pulo do gato */
+  public Boolean bucascando(String texto) {
     tamanhoPalavra = this.palavra.length();
-    tamanhoFrase = this.texto.length();
+    tamanhoFrase = texto.length();
     valorPalavra = hash(this.palavra);
 
-    for(int x = tamanhoPalavra,y = 0; x <= tamanhoFrase;x++, y++){     
-      temp = this.texto.substring(y,x);
+    for (int x = tamanhoPalavra, y = 0; x <= tamanhoFrase; x++, y++) {
+      temp = texto.substring(y, x);
       valorFrase = hash(temp);
-      if(valorFrase.equals(valorPalavra)){
+      if (valorFrase.equals(valorPalavra)) {
         return true;
       }
     }
     return false;
   }
 
-  /* Medtodo para fazer as palavras serem unicas*/
+  /* for no array de nomes */
+  public boolean ler() throws IOException {
+    Path path = Paths.get("C:\\Users\\Gustavo\\Desktop\\Estrutura de Dados\\Trabalho\\1H4.txt");
+    List<String> linhasArquivo = Files.readAllLines(path);
+    for (String linha : linhasArquivo) {
+        if(bucascando(linha)){
+          return true;
+        }
+    }
+    return false;
+  }
 
-
+  /* Medtodo para fazer as palavras serem unicas */
 
   public int hash(String mudar) {
     int ascii = 1;
     for (Integer i = 0; i < mudar.length(); i++) {
-      char character = mudar.charAt(i);    
-       ascii *= (int) character; 
-   }
-    if(ascii < 0){
+      char character = mudar.charAt(i);
+      ascii *= (int) character;
+    }
+    if (ascii < 0) {
       ascii *= -1;
     }
     return ascii;
   }
 
-  /*Get and set */
+  /* Get and set */
   public Integer getTamanhoPalavra() {
     return this.tamanhoPalavra;
   }
@@ -69,7 +84,7 @@ public class RabinKarp {
     return this.multipicado;
   }
 
-  public void setMultipicado( Integer multipicado) {
+  public void setMultipicado(Integer multipicado) {
     this.multipicado = multipicado;
   }
 
