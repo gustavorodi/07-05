1cd6a139ebdcdaefc2c45fdda5353ba9f59b6414
gustavorodi <37717445+gustavorodi@users.noreply.github.com>
2019-05-27 23:13:13 -0300

    Atualização

--- Main.java
+++ Main.java
@@ -8,10 +8,13 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) throws IOException {
 
+
+
         Scanner leitor = new Scanner(System.in);
         System.out.print("Escolha uma palavra para ser utilizada para pesquisa:\n");
         String chaveDeProdura = leitor.nextLine();
 
+        BoyerMoore testeBoyerMoore = new BoyerMoore("amor","chuva de amor");
         RabinKarp testeRabinKarp = new RabinKarp(chaveDeProdura);
         Naive testNaive = new Naive(chaveDeProdura);
         Time contando = new Time();
@@ -28,9 +31,18 @@ public class Main {
         System.out.println("Iniciando a busca usando o Rabinkarp...");
         contando.comecar();
         testeRabinKarp.ler();
-        contando.parar("Naive");
-        System.out.println("Fim da busca usando o RabinKary! ");
+        contando.parar("Rabinkarp");
+        System.out.println("Fim da busca usando o RabinKarp! ");
         System.out.println("**************************************");
+
+        System.out.println("**************************************");
+        System.out.println("Iniciando a busca usando o BoyerMoore...");
+ 
+        testeBoyerMoore.darMach();
+      
+        System.out.println("Fim da busca usando o BoyerMoore! ");
+        System.out.println("**************************************");
+        
 
     }
 
